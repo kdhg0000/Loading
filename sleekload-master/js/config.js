957570519848cd0b837564b566dc8bfbaa2d0f66
kdhg0000 <35376035+kdhg0000@users.noreply.github.com>
2018-01-22 00:42:29 +0900

    1

--- sleekload-master/js/config.js
+++ sleekload-master/js/config.js
@@ -9,7 +9,10 @@
 
 /*URL for the server logo
 Leave it blank to use text instead*/
-var l_serverImage = "";
+var l_serverImage = "https://raw.githubusercontent.com/kdhg0000/Loading/0464cc64/sleekload-master/backgrounds/images/1.jpg",
+	                "https://cdn.rawgit.com/kdhg0000/Loading/0464cc64/sleekload-master/backgrounds/images/2.jpg",
+	                "https://cdn.rawgit.com/kdhg0000/Loading/0464cc64/sleekload-master/backgrounds/images/3.jpg",
+					;
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
@@ -84,9 +87,23 @@ var l_musicDisplay = true;
 /*Music playlist
 Place how much Youtube IDs/.ogg you want*/
 var l_musicPlaylist = [
-    {youtube: "60ItHLz5WEA", name: "[Alan walker]-Fade (No copy right sounds release)"},
-		{youtube: "1-xGerv5FOk", name:"[Alan walker]-Alon (No copy right sounds release)"},
-			{youtube: "xshEZzpS4CQ",name:"[Alan walker]-Force (no copy right sounds release)"},
+  {youtube: "kyg1ccTfW5s", name: "[Blend・S]- 8bit."},
+		{youtube: "5eWE06pvzfs", name: "[Daydream Sky Train - IA, Hatsune Miku ft. Orangestar]"	},
+			{youtube: "60ItHLz5WEA", name: "[Alan walker]-Fade (No copy right sounds release)"},
+				{youtube: "1-xGerv5FOk", name:"[Alan walker]-Alon (No copy right sounds release)"},
+					{youtube: "xshEZzpS4CQ",name:"[Alan walker]-Force (no copy right sounds release)"},
+	                    {youtube: "VKzWLUQizz8&index=116&list=PL59ILWYueEqanRXeYVLuhtGA59sVVSFFJ",name:"Justice - Genesis - †"},
+                        	{youtube:"=veC1Z2ZDtFA",name:"This Game - No Game No Life OP"},
+	                            {youtube:"OSPI1qhBB_8",name:"THERE IS A REASON-No Game No Life ZERO Them"},
+	                                {youtube:"jvipPYFebWc",name:"Roundtable Rival - Lindsey Stirling"},
+	                                    {youtube:"5O9-WAkSwQk",name:"Nightcore - The Calling"},
+	                                        {youtube:"6FNHe3kf8_s",name:"Different Heaven - Nekozilla"},
+                                                {youtube:"XogSflwXgpw",name:"【IA】アスノヨゾラ哨戒班【オリジナル】-(내일의 밤하늘 초계반)"},
+	                                                {youtube:"emrt46SRyYs",name:"DAYBREAK FRONTLINE / Orangestar feat.IA"},
+														{youtube:"lYBfWxQRM_A",name:"Keith Kenniff - Goldengrove"},
+															{youtube:"2OdgUuH9bm0",name:"Maggie Lindemann - Pretty Girl"},
+];
+
 ];
 
 /*Random music order?*/
